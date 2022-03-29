9aba81245dd008bcb48cf62007266495a9b4042e
Surya Verma <72555711+00surya@users.noreply.github.com>
2022-03-29 21:27:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Author: liuhh02 https://medium.com/@liuhh02
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup,ParseMode
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
@@ -20,11 +21,48 @@ TOKEN = '5072195132:AAFD1G5nOQkAtLkddqVzIO0gpBzh2_1WTDo'
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    topic_keyboard = [['/start','/get']]
+    reply_markup = ReplyKeyboardMarkup(keyboard=topic_keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+#     update.message.reply_text('Hi!')
+#     help_txt = "Hey! Do you want help?"
+    # bot.send_message(chat_id=update.message.chat_id, text=help_txt)
+    res = "this bot is designed to fetch upcoming coding contests details, to do so click on the button 'get' given below."
+    update.message.reply_text(res,reply_markup=reply_markup)
+#     bot.send_message(chat_id=update.message.chat_id,text=res,reply_markup=ReplyKeyboardMarkup(keyboard=topic_keyboard,one_time_keyboard=True))
+
+
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
+   
+def get(bot,update):
+  """Send a message when the command /get is issued."""
+    contest_api_url = "https://kontests.net/api/v1/all"
+    res = requests.get(contest_api_url)
+    res_status = res.status_code
+
+    if res_status == 200:
+        res = res.json()
+        corpus = ""
+        i = 1
+        for contest in res:
+            if i % 10 == 0:
+                bot.send_message(chat_id=update.message.chat_id,text=corpus,parse_mode=ParseMode.HTML,disable_web_page_preview=True)
+                corpus = ""
+
+            contest_name = contest["name"]
+            date = contest["start_time"][:10]
+            time_ = contest["start_time"][11:16]
+            time_ = datetime.strptime(time_, "%H:%M").strftime("%r")
+            url = contest['url']
+            site = contest['site']
+            text = f"#{i}: {site} - <a href='{url}'>{contest_name}</a> \n \t date - {date}, time - {time_[:5]+time_[8:]} \n"
+            corpus += text
+            i+=1
+
 
 def echo(update, context):
     """Echo the user message."""
@@ -46,7 +84,7 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("get", get))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
